69f902afeadf24e8620da79c39bc67de3d979177
Cankun Wang <flykun0620@gmail.com>
2021-09-26 20:56:43 -0400

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { SearchOutlined } from '@ant-design/icons'
 import SmileOutlined from '@ant-design/icons/lib/icons/SmileOutlined'
-import { Button } from 'antd'
+import { Button, Divider } from 'antd'
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 
 import Welcome from '~/Welcome'
 
@@ -9,6 +10,42 @@ import { MoonIcon } from './icons/Moon'
 import logo from './logo.svg'
 
 function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen text-center dark:bg-gray-700 bg-white">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Divider />
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+function Home() {
   const [count, setCount] = useState(0)
 
   function toggleDarkMode() {
@@ -24,31 +61,30 @@ function App() {
       document.documentElement.classList.add('dark')
     }
   }
-
   return (
-    <div className="flex flex-col min-h-screen text-center bg-white dark:bg-gray-700">
-      <nav className="flex items-center justify-end h-32 px-16">
+    <div>
+      <nav className="flex items-center justify-end px-16 h-32">
         <button
-          className="flex p-2 text-black rounded hover:text-yellow-400 dark:text-white focus:outline-none hover:bg-gray-700 dark:hover:bg-gray-500 dark:hover:text-yellow-400"
+          className="dark:hover:bg-gray-500 dark:hover:text-yellow-400 flex p-2 text-black dark:text-white hover:text-yellow-400 hover:bg-gray-700 rounded focus:outline-none"
           onClick={toggleDarkMode}>
           <MoonIcon></MoonIcon>
         </button>
       </nav>
-      <header className="flex flex-col items-center justify-center flex-grow text-2xl text-gray-700 dark:text-white">
-        <img src={logo} className="animate-spin-slow h-48" alt="logo" />
+      <header className="flex flex-col flex-grow items-center justify-center text-gray-700 dark:text-white text-2xl">
+        <img src={logo} className="h-48 animate-spin-slow" alt="logo" />
         <Welcome name="World"></Welcome>
         <p>Vite + React!</p>
-        <div className="text-center mb-5">
+        <div className="mb-5 text-center">
           <SmileOutlined
             aria-label="this is a icon"
             twoToneColor="#eb2f96"
             height="2em"
-            className="logo mr-0 mt-2 animate-spin-slow "
+            className="logo mr-0 mt-2 animate-spin-slow"
           />
         </div>
         <div className="my-8">
           <button
-            className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+            className="inline-flex items-center px-6 py-3 text-white text-base font-medium bg-teal-600 hover:bg-teal-700 border border-transparent rounded-md focus:outline-none shadow-sm focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
             onClick={() => setCount((count) => count + 1)}>
             Count is: {count}
           </button>
@@ -84,4 +120,11 @@ function App() {
   )
 }
 
+function About() {
+  return <h2>About</h2>
+}
+
+function Users() {
+  return <h2>Users</h2>
+}
 export default App
